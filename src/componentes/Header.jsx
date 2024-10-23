ec23b62b19311f17db94655dcd906336907ee7da
Lucas Matias <lucasmatiassilva2022@gmail.com>
2024-10-23 13:50:18 -0300

    feat: adição do logo STOORE

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,9 +1,10 @@
+import StooreLogo from '../assets/stoore_logo_white.svg';
+
 const Header = () => {
   return (
-    <nav className="flex flex-row justify-center items-center fixed top-0 bg-white w-full h-auto border-b-2 border-primary">
+    <nav className="flex flex-row justify-center items-center fixed top-0 w-full h-auto bg-primary">
       <div className="flex flex-col items-center justify-center my-4">
-        STOORE
-        <img src="" alt="" className="" />
+        <img src={StooreLogo} alt="" className="" />
       </div>
     </nav>
   );
