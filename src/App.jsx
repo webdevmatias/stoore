984474ee17fac8f2775fdaeef9f7d46396b28ae8
Lucas Matias <lucasmatiassilva2022@gmail.com>
2024-10-23 13:40:59 -0300

    fix: nav icons size {home e carrinho}

--- src/App.jsx
+++ src/App.jsx
@@ -32,10 +32,10 @@ function App() {
         
         <nav className="flex flex-row justify-around fixed bottom-0 bg-primary w-4/5 h-auto rounded-t-lg">
           <div className="flex flex-col items-center justify-center shadow-sm w-1/3" onClick={() => setCurrentSection('home')}>
-            <FaHome className="text-white text-2xl md:text-3xl my-4" /> {/* Ícone menor na versão mobile */}
+            <FaHome className="text-white text-3xl md:text-3xl my-4" /> {/* Ícone menor na versão mobile */}
           </div>
           <div className="flex flex-col items-center justify-center shadow-sm w-1/3" onClick={() => setCurrentSection('carrinho')}>
-            <FaShoppingCart className="text-white text-2xl md:text-3xl my-4" /> {/* Ícone menor na versão mobile */}
+            <FaShoppingCart className="text-white text-3xl md:text-3xl my-4" /> {/* Ícone menor na versão mobile */}
           </div>
           <div className="flex flex-col items-center justify-center shadow-sm w-1/3" onClick={() => setCurrentSection('perfil')}>
             <FaUserAlt className="text-white text-2xl md:text-2xl my-4" /> {/* Ícone menor na versão mobile */}
