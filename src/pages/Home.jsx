68d8192133ec71f680cfa7310de12dbd4b879caa
Lucas Matias <lucasmatiassilva2022@gmail.com>
2024-10-26 21:47:06 -0300

    feat: adicão das imagens dos notebooks em assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,98 +2,95 @@ import "../App.css";
 import ImageSlider from "../componentes/ImageSlider";
 import ProductCard from "../componentes/ProductCard";
 
+// Imagens para o slider
 import slide1 from "../assets/slide1.png";
 import slide2 from "../assets/slide2.png";
 import slide3 from "../assets/slide3.png";
 import slide4 from "../assets/slide4.png";
 
-/*
-const product = {
-  id: "1",
-  image: "slide1",
-  name: "Notebook Dell Inspiron 15 - Intel Core i5, 8GB RAM, 256GB SSD",
-  description: "Notebook de alto desempenho com processador Intel Core i5, 8GB de RAM e armazenamento SSD de 256GB.",
-  price: 2999.99,
-  rating: 4.5,
-  stock: null, // ou Infinity, indicando estoque ilimitado
-  categories: [
-      { type: "Processador", value: "Intel Core i5" },
-      { type: "Memória RAM", value: "8GB" },
-      { type: "Armazenamento", value: "256GB SSD" },
-      { type: "Tela", value: "15.6'' Full HD" }
-  ]
-};
-*/
+// Imagens dos notebooks
+import notebook1 from "../assets/notebooks-stoore/notebook-1.png";
+import notebook2 from "../assets/notebooks-stoore/notebook-2.png";
+import notebook3 from "../assets/notebooks-stoore/notebook-3.png";
+import notebook4 from "../assets/notebooks-stoore/notebook-4.png";
+import notebook5 from "../assets/notebooks-stoore/notebook-5.png";
+import notebook6 from "../assets/notebooks-stoore/notebook-6.png";
+import notebook7 from "../assets/notebooks-stoore/notebook-7.png";
+import notebook8 from "../assets/notebooks-stoore/notebook-8.png";
+import notebook9 from "../assets/notebooks-stoore/notebook-9.png";
+import notebook10 from "../assets/notebooks-stoore/notebook-10.png";
+import notebook11 from "../assets/notebooks-stoore/notebook-11.png";
+import notebook12 from "../assets/notebooks-stoore/notebook-12.png";
 
 const products = [
   {
-    image: slide1,
+    image: notebook1,
     name: "Notebook Dell Inspiron 15 - Intel Core i5, 8GB RAM, 256GB SSD",
     price: 2999.99,
     rating: 4.5,
   },
   {
-    image: slide2,
+    image: notebook2,
     name: "Notebook Lenovo Ideapad 3 - AMD Ryzen 5, 8GB RAM, 512GB SSD",
     price: 2399.99,
     rating: 4.0,
   },
   {
-    image: slide3,
+    image: notebook3,
     name: "Notebook Acer Nitro V15 - Intel Core i7, 16GB RAM, 1TB HDD",
     price: 2299.99,
     rating: 4.8,
   },
   {
-    image: slide4,
+    image: notebook4,
     name: "Notebook Acer Nitro V15 - Intel Core i5, 8GB RAM, 512GB SSD",
     price: 3999.99,
     rating: 4.2,
   },
   {
-    image: slide1,
+    image: notebook5,
     name: "Notebook ASUS ZenBook 14 - Intel Core i7, 16GB RAM, 512GB SSD",
     price: 4999.99,
     rating: 4.6,
   },
   {
-    image: slide2,
+    image: notebook6,
     name: "Notebook HP Pavilion 15 - AMD Ryzen 5, 8GB RAM, 1TB HDD",
     price: 3799.99,
     rating: 4.3,
   },
   {
-    image: slide3,
+    image: notebook7,
     name: "Notebook Dell XPS 13 - Intel Core i7, 16GB RAM, 1TB SSD",
     price: 6999.99,
     rating: 4.9,
   },
   {
-    image: slide4,
+    image: notebook8,
     name: "Notebook Lenovo ThinkPad T14 - Intel Core i7, 16GB RAM, 512GB SSD",
     price: 4699.99,
     rating: 4.7,
   },
   {
-    image: slide1,
+    image: notebook9,
     name: "Notebook Acer Aspire 5 - Intel Core i5, 8GB RAM, 256GB SSD",
     price: 2399.99,
     rating: 4.1,
   },
   {
-    image: slide2,
+    image: notebook10,
     name: "Notebook MSI GF63 - Intel Core i5, 8GB RAM, 512GB SSD",
     price: 3799.99,
     rating: 4.4,
   },
   {
-    image: slide3,
+    image: notebook11,
     name: "Notebook Razer Blade 15 - Intel Core i7, 16GB RAM, 1TB SSD",
     price: 7999.99,
     rating: 4.8,
   },
   {
-    image: slide4,
+    image: notebook12,
     name: "Notebook HP Omen 15 - AMD Ryzen 7, 16GB RAM, 1TB SSD",
     price: 4999.99,
     rating: 4.5,
@@ -101,13 +98,13 @@ const products = [
 ];
 
 const Home = () => {
-  const images = [slide1, slide2, slide3, slide4];
+  const sliderImages = [slide1, slide2, slide3, slide4];
 
   return (
     <>
       <div className="flex flex-col items-center w-full h-full">
         <section className="flex flex-col justify-center w-full items-center mt-16">
-          <ImageSlider images={images} />
+          <ImageSlider images={sliderImages} />
         </section>
         
         <section className="flex flex-row flex-wrap justify-center mt-4 mb-20 w-full">
